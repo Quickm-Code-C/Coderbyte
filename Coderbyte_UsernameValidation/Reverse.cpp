464434331646506770e3d3d5d670bec124071302
quickm.career <quickm.career@gmail.com>
2021-01-18 14:16:24 -0800

    For challenge text for Alphabet Soup and First Reverse.

--- Coderbyte_UsernameValidation/Reverse.cpp
+++ Coderbyte_UsernameValidation/Reverse.cpp
@@ -1,5 +1,10 @@
 #include "Reverse.h"
 
+// Have the function FirstReverse(str) take the str parameter being passed and 
+// return the string in reversed order. For example: if the input string is 
+// "Hello World and Coders" then your program should return the string 
+// sredoC dna dlroW olleH.
+
 std::string Reverse::FirstReverse(std::string str)
 {
 	std::reverse(str.begin(), str.end());
