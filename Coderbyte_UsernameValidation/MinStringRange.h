a0725f1f04dda6b9643f7a8c6f55da6c7e02baf2
Michael D Quick <quickm.career@gmail.com>
2021-01-22 14:14:05 -0800

    Update MinStringRange.h

--- Coderbyte_UsernameValidation/MinStringRange.h
+++ Coderbyte_UsernameValidation/MinStringRange.h
@@ -8,8 +8,8 @@ class MinStringRange
 public:
 	std::string minWindowSubstring(std::string strArr[], int arrLength);
 protected:
-	std::map<char, int>				 createCountMap(const std::string s);
+	std::map<char, int>		         createCountMap(const std::string s);
 	std::vector<std::string>		 extractWindows(const std::string str, int length);
-	bool							 isValidWindow(const std::string window, std::map<char, int> patternMap);
+	bool					 isValidWindow(const std::string window, std::map<char, int> patternMap);
 };
 
